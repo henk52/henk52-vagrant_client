eb4cfb08687f9283a757cba925e6ed24d15a2918
henk52 <henk52@users.noreply.github.com>
2013-12-22 17:27:00 +0100

    pkg_gen.rb: Added comments.

--- files/_pkg_gen/pkg_gen.rb
+++ files/_pkg_gen/pkg_gen.rb
@@ -1,11 +1,16 @@
 #!/usr/bin/ruby
 
+# This program generates the OS package code based on the Puppet Modulefile.
+#  Currently it only supports Fedora.
+
+# Author::    Henk52  (mailto:henk52@users.noreply.github.com)
+# Copyright:: Copyright (c) 2013 henk52.
+# License::   Apache License, Version 2.0
+
+# Finding out what the absolute path for this script, is.
 f_szScriptFullPath = File.expand_path(File.dirname(__FILE__))
 
 arSplitPath = f_szScriptFullPath.split('/')
-
-# reverse the array in place.
-#arSplitPath.reverse!
 
 # TODO V exit on error if the array doesn't contain atleast 3 elements.
 #    module_path/files/_pkg_gen
@@ -16,23 +21,28 @@ arSplitPath.pop
 # remove the 'files' directory
 arSplitPath.pop
 
+# Create the new path string.
 f_szAbsolutePathToModule = arSplitPath.join('/')
 
+# Read the Modulefile into an array, on line per entry in the array.
 arLinesRead = IO.readlines("#{f_szAbsolutePathToModule}/Modulefile")
 
+# This hash will contain all the key/value entries in the Modulefile.
 hModulefileInformation =  Hash.new()
 
+# Var set when the loop is investigating multilines.
 nHandleMultiline = 0
 
 szKey = ""
 szValue = ""
 szQuoteCharacter = ""
 
+# Go through each of the raw lines.
 arLinesRead.each { |szRawLine|
   # remove the newline at the end and remove and starting or trailing white spaces.
   szLine = (szRawLine.chomp()).strip()
 
-  # only handle the line if it is not a comment and it is not empty.
+  # only handle the szLine if it is not a comment and it is not empty.
   if ( ( szLine !~ /^#/ ) && ( szLine !~ /^$/ ) ) then
     if ( nHandleMultiline == 0 ) then
       # Split on the first whitespace.
