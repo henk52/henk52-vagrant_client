e31ecaf55077f473439e60182b00ca05028cc790
henk52 <henk52@users.noreply.github.com>
2013-12-22 17:03:58 +0100

    pkg_gen.rb: Supports multiline strings.

--- files/_pkg_gen/pkg_gen.rb
+++ files/_pkg_gen/pkg_gen.rb
@@ -22,17 +22,41 @@ arLinesRead = IO.readlines("#{f_szAbsolutePathToModule}/Modulefile")
 
 hModulefileInformation =  Hash.new()
 
+nHandleMultiline = 0
+
+szKey = ""
+szValue = ""
+szQuoteCharacter = ""
+
 arLinesRead.each { |szRawLine|
   # remove the newline at the end and remove and starting or trailing white spaces.
   szLine = (szRawLine.chomp()).strip()
 
   # only handle the line if it is not a comment and it is not empty.
   if ( ( szLine !~ /^#/ ) && ( szLine !~ /^$/ ) ) then
-#    (szKey, szValue) = szLine.split(%r{\s+})
-    (szKey, szSepparator, szValue) = szLine.partition(%r{\s+})
-puts "Key: =#{szKey}=, Value: =#{szValue}="
-    # replace all ' with " in szValue.
-    hModulefileInformation[szKey] = szValue.gsub(/'/, '"')
-#    puts hModulefileInformation[szKey]
+    if ( nHandleMultiline == 0 ) then
+      # Split on the first whitespace.
+      (szKey, szSepparator, szValue) = szLine.partition(%r{\s+})
+      # TODO Barf if first character, in szValue, isn't ' or ".
+      szQuoteCharacter = szValue[0]
+
+      # We don't know that we are in multiline but we are guessing.
+      nHandleMultiline = 1
+    else
+      szValue += szLine
+    end
+
+    puts "Length: #{szValue.length()}, Quoute: #{szQuoteCharacter}, Last char: #{szValue[szValue.length()-1]}"
+#    puts "  szValue=#{szValue}="
+    if ( szValue[szValue.length()-1] == szQuoteCharacter ) then
+      nHandleMultiline = 0
+    end    
+    
+    # If we are no longer handling multiline, then save the key.
+    if ( nHandleMultiline == 0 ) then
+      puts "Key: =#{szKey}=, Value: =#{szValue}="
+      # replace all ' with " in szValue.
+      hModulefileInformation[szKey] = szValue
+    end
   end
 }
